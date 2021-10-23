f8989d256bad2d4f8d57919d2918d806c3cdff88
Wojciech <wojciech05521@gmail.com>
2021-10-23 19:21:41 +0200

    3

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -136,6 +136,7 @@ public class World {
         Vector2d position2 = new Vector2d(-2, 1);
         System.out.println(position2);
         System.out.println(position1.add(position2));
+        int o;
 
     }
 }
