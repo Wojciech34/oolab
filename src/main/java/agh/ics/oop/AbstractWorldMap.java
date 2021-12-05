b9f221e299858362eb21ed460c3874e3d5a68fc0
Wojciech <wojciech05521@gmail.com>
2021-12-05 18:35:08 +0100

    7a

--- src/main/java/agh/ics/oop/AbstractWorldMap.java
+++ src/main/java/agh/ics/oop/AbstractWorldMap.java
@@ -8,8 +8,8 @@ import java.util.Map;
 abstract class AbstractWorldMap implements IWorldMap, IPositionChangeObserver {
     //protected LinkedList<Animal> map_a = new LinkedList<>();
     protected Map<Vector2d, Animal> map_a = new HashMap<>();
-    protected Vector2d min_left = new Vector2d(0,0);
-    protected Vector2d max_right = new Vector2d(0,0);
+  //  protected Vector2d min_left = new Vector2d(0,0);
+ //   protected Vector2d max_right = new Vector2d(0,0);
     public String toString(){
         Vector2d vector1 = giveCorner1();
         Vector2d vector2 = giveCorner2();
@@ -22,15 +22,15 @@ abstract class AbstractWorldMap implements IWorldMap, IPositionChangeObserver {
     public void positionChanged(Vector2d oldPosition, Vector2d newPosition) {
         map_a.put(newPosition, map_a.get(oldPosition));
         map_a.remove(oldPosition);
-        min_left = newPosition.lowerLeft(min_left);
-        max_right = newPosition.upperRight(max_right);
+ //       min_left = newPosition.lowerLeft(min_left);
+  //      max_right = newPosition.upperRight(max_right);
     }
     @Override
     public boolean place(Animal animal) throws IllegalArgumentException{
         if (canMoveTo(animal.position)){
             map_a.put(animal.position, animal);
-            min_left = animal.position.lowerLeft(min_left);
-            max_right = animal.position.upperRight(max_right);
+ //           min_left = animal.position.lowerLeft(min_left);
+  //          max_right = animal.position.upperRight(max_right);
             animal.addObserver(this);
             return true;
         }
